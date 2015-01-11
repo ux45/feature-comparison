4df6dc7507267d44e30c3af912b3f611f1330dd5
Ian C <ian@ik2.us>
2015-01-10 21:57:15 -0800

    Merged CSS files; updated feature selection table and front page; add JS to improve feature selection UI.

--- js/featcomp.js
+++ js/featcomp.js
@@ -5,5 +5,16 @@ jQuery(document).ready(function()
     jQuery("a.menu-616").wrapInner("<span></span>").prepend("<i class='fa fa-file-text-o'></i> ");
     jQuery("a.menu-592").wrapInner("<span></span>").prepend("<i class='fa fa-list-ul'></i> ");
     jQuery("a.menu-777").wrapInner("<span></span>").prepend("<i class='fa fa-wrench'></i> ");
+    jQuery(".pagination").before('<div class="alert alert-warning" id="selected-warning" role="alert">Please add selected before leaving this page</div>');
+		jQuery("#selected-warning").hide();
+		jQuery(".vbo-views-form #edit-select").hide();
+		jQuery(".vbo-views-form input:checkbox").click(function() {
+	    if(jQuery(this).is(":checked")) {
+	      jQuery(".vbo-views-form #edit-select").show(300);
+	      jQuery("#selected-warning").show(300);
+	    } else {
+				jQuery(".vbo-views-form #edit-select").hide(200);
+	    }
+		});
   } 
 ); 
